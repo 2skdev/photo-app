6a9c7bbfb0cfce13c0920996a8a10212232e6fe5
tosaki <t.chay9271@gmail.com>
2024-08-21 19:06:21 +0900

    fix base url for production

--- src/app/(admin)/admin/env/page.tsx
+++ src/app/(admin)/admin/env/page.tsx
@@ -1,6 +1,9 @@
+import { baseUrl } from "@/constants/url";
+
 export default function Page() {
   return (
     <>
+      <div>baseUrl = {baseUrl}</div>
       {Object.entries(process.env)
         .filter(([key]) => key.startsWith("NEXT_PUBLIC"))
         .map(([key, value]) => (
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,11 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+export const baseUrl = (() => {
+  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    case "production":
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+    case "preview":
+    case "development":
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    default:
+      return "http://localhost:3000";
+  }
+})();
