c8a448f3cf77bf01315f83b7f73c3021d9d50117
tosaki <t.chay9271@gmail.com>
2024-09-10 20:55:58 +0900

    add modal overflow

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -62,7 +62,7 @@ export function ModalProvider() {
           className="fixed inset-0 z-10 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50"
           onClick={onBackdropClick}
         >
-          <div className="relative z-10 h-screen w-screen rounded bg-base-100 p-4 md:h-auto md:w-auto">
+          <div className="relative z-10 h-screen w-screen overflow-y-auto rounded bg-base-100 p-4 md:h-auto md:w-auto">
             {item.child}
           </div>
         </div>
