9b3f0a80b0a2801eb1794779b88c90779664ccea
tosaki <t.chay9271@gmail.com>
2024-09-12 12:57:04 +0900

    fix nextjs warning

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,5 +1,5 @@
 export function Progress() {
   return (
-    <progress className="progress progress-primary fixed inset-0 z-50 h-1 w-screen rounded-none"></progress>
+    <progress className="progress progress-primary absolute inset-0 z-50 h-1 w-screen rounded-none"></progress>
   );
 }
