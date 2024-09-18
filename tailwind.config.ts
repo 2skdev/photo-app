5f37aa9de73accc93182c229786110ada78a10ce
tosaki <t.chay9271@gmail.com>
2024-09-18 22:59:19 +0900

    wip fix tailwind build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import daisyui from "daisyui";
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
